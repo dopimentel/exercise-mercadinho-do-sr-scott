55f50bf5cc410565ff6cf1c45da2fb05d5f33983
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-23 16:32:02 -0300

    req 06

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,7 @@
+import { CartItem } from './types/CartItem';
+import { Drink } from './types/Drink';
+import { Food } from './types/Food';
+
 export function first<T>(array: T[]): T {
   return array[0];
 }
@@ -8,4 +12,12 @@ export function updateItem <T>(array: T[], index: number, object: T): T[] {
     return array;
   }
   return array;
+}
+
+export function biuldCartItem({ name, price }: Drink | Food, quantity: number): CartItem {
+  return {
+    name,
+    price,
+    quantity,
+  } as CartItem;
 }
